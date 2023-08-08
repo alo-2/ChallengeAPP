cd89a24b58c597ccc19e31be9e47a8da5b98a215
Jacek Lesniewski <jacek.lesniewski@gmail.com>
2023-08-08 14:04:22 +0200

    zadanie dzien 4

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Pozdrawiam Jacek");
+﻿String imie = "Łukasz";
+char plec = 'M';
+int wiek = 17;
+druk();
+imie = "Ewa";
+plec = 'K';
+wiek = 33;
+druk();
+imie = "Łukasz";
+plec = 'M';
+wiek = 21;
+druk();
+imie = "Łukasz";
+plec = 'K';
+wiek = 72;
+druk();
+void druk()
+{
+    if (wiek < 30 && plec == 'K')
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (imie == "Ewa" && wiek == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (plec == 'M' && wiek < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else if (imie == "Łukasz" && plec == 'M' && wiek == 21)
+    {
+        Console.WriteLine("Mężczyzna pełnoletni o imieniu Łukasz mający 21 lat");
+    }
+
+    else if (wiek >= 65)
+    {
+        Console.WriteLine("Emeryt/ka");
+    }
+}
